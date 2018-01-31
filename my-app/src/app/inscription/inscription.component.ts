e366e1e928d06410db8c9e5f776b973ae27143d6
Kewou <joel.noumia@adventiel.fr>
2018-01-31 17:00:43 +0100

    Mise en place de la validation formulaire

--- my-app/src/app/inscription/inscription.component.ts
+++ my-app/src/app/inscription/inscription.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../users/modele/user';
 import {UserService} from '../users/service/user.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -24,11 +24,12 @@ export class InscriptionComponent implements OnInit {
     private router: Router,
   ) {
       this.inscriptionForm = formBuilder.group({
-        'email': [null, Validators.required,Validators.email],
-        'password': [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(20)])],
-        'confirmPass': [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(20)])],
+        'emailCtrl': [null, Validators.compose([Validators.required,Validators.pattern("^[a-z]+@[a-z0-9.-]+\.[a-z]{2,4}$")])],
+        'passwordCtrl': [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(20)])],
+        'confirmPassCtrl': [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(20)])],
         'validate': ''
-      });
+      },{validator: this.checkIfMatchingPasswords('passwordCtrl', 'confirmPassCtrl')});
+       );
    }
 
   submitted = false;
@@ -45,6 +46,10 @@ export class InscriptionComponent implements OnInit {
 
       (validate) => {
 
+        if( this.inscriptionForm.get('password') !== this.InscriptionComponent.get('confirmPass')) {
+
+        }
+
         if (validate === '1') {
           this.inscriptionForm.get('email').setValidators([Validators.required, Validators.minLength(3)]);
           this.titleAlert = 'You need to specify at least 3 characters';
@@ -54,8 +59,21 @@ export class InscriptionComponent implements OnInit {
         this.inscriptionForm.get('confirmPass').updateValueAndValidity();
 
       });
-
   }
+
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+          return (group: FormGroup) => {
+            let passwordInput = group.controls[passwordKey],
+                passwordConfirmationInput = group.controls[passwordConfirmationKey];
+            if (passwordInput.value !== passwordConfirmationInput.value) {
+              return passwordConfirmationInput.setErrors({notEquivalent: true})
+            }
+            else {
+                return passwordConfirmationInput.setErrors(null);
+            }
+          }
+  }
+
 
   addUser(user){
     this.email = user.email;
